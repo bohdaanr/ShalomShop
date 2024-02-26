c77ce590a8158c4c96b8488d7f2e322753306ba8
Victor Hrygorash <viktor12041991@i.ua>
2024-02-26 18:07:25 +0200

    added models last zemblaze 7 lite and removed top

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -6,12 +6,6 @@ export const Main = () => {
     <div className="main">
       <div className="container">
         <div className="main__inner">
-          <div className="main__top">
-            <h1 className="main__title">SHALOM SHOP</h1>
-            <p className="main__text">
-              SHALOM SHOP- інтернет-магазин, де ви можете підібрати годинники, які будуть підкреслювати вашу індивідуальність та аксесуари під них
-            </p>
-          </div>
           <div className="main__bottom">
             <div className="main__cards">
               {models.map(m => (
@@ -19,7 +13,7 @@ export const Main = () => {
                   <div className="main__card--top">
                     <img src={m.img} alt={m.name} className="main__card--img" />
                   </div>
-                  <h3 className="main__card--title">{m.name} - <b>{m.price}</b></h3>
+                  <h3 className="main__card--title">{m.name} - <b>{m.price}грн</b></h3>
                   <div className="main__card--bottom">
                     <span className="main__card--bottom-item">Дисплей:{m.display}</span>
                     <span className="main__card--bottom-item">Росширення:{m.resolution}</span>
