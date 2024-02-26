94816bf31bfc9f3782aabd22f383d337ff929b64
Victor Hrygorash <viktor12041991@i.ua>
2024-02-26 11:43:15 +0200

    added main

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,30 +1,36 @@
 import './Main.scss';
-
+import {models} from '../../assets/models';
 
 export const Main = () => {
   return (
     <div className="main">
       <div className="container">
-        <div className="logos">
-          <div className="logos-slide">
-            <img src="./img/models/ColmiP71-black.jpg" alt="" />
-            <img src="./img/models/ColmiP71-blue.jpg" alt="" />
-            <img src="./img/models/ColmiP71-purple.jpg" alt="" />
-            <img src="./img/models/ColmiP71-white.jpg" alt="" />
-            <img src="./img/models/ColmiP73-black.jpg" alt="" />
-            <img  src="./img/models/ColmiP73-orange.jpg" alt="" />
+        <div className="main__inner">
+          <div className="main__top">
+            <h1 className="main__title">SHALOM SHOP</h1>
+            <p className="main__text">
+              SHALOM SHOP- інтернет-магазин, де ви можете підібрати годинники, які будуть підкреслювати вашу індивідуальність та аксесуари під них
+            </p>
           </div>
-          <div className="logos-slide">
-            <img src="./img/models/ColmiP71-black.jpg" alt="" />
-            <img src="./img/models/ColmiP71-blue.jpg" alt="" />
-            <img src="./img/models/ColmiP71-purple.jpg" alt="" />
-            <img src="./img/models/ColmiP71-white.jpg" alt="" />
-            <img src="./img/models/ColmiP73-black.jpg" alt="" />
-            <img  src="./img/models/ColmiP73-orange.jpg" alt="" />
+          <div className="main__bottom">
+            <div className="main__cards">
+              {models.map(m => (
+                <div className="main__card" key={m.id}>
+                  <div className="main__card--top">
+                    <img src={m.img} alt={m.name} className="main__card--img" />
+                  </div>
+                  <h3 className="main__card--title">{m.name} - <b>{m.price}</b></h3>
+                  <div className="main__card--bottom">
+                    <span className="main__card--bottom-item">Дисплей:{m.display}</span>
+                    <span className="main__card--bottom-item">Росширення:{m.resolution}</span>
+                    <span className="main__card--bottom-item">Ємність батареї:{m.battery} А.г</span>
+                  </div>
+                </div>
+              ))}
+            </div>
           </div>
-       </div>
+        </div>
       </div>
-
     </div>
   )
 }
